714ac70dd822d18cbd0f228bfa1535901f1c8872
Safwan146 <Safwanalsadaf17@gmail.com>
2025-05-02 19:09:12 +0600

    Add files via upload

---
+++ untitled..py
@@ -0,0 +1,8 @@
+# hello_safwan.py
+
+def greet():
+    name = "Safwan"
+    print(f"Hello, {name}! This is your first Python script from GitHub to Termux!")
+
+if __name__ == "__main__":
+    greet()
